dce5471c8995d32c33cdd9ec72c041f5607d6d9a
yenanfei <yenanfei@momenta.ai>
2021-11-19 20:10:58 +0800

    first commit

--- main.py
+++ main.py
@@ -72,95 +72,95 @@ class Dataset(BaseDataset):
         # masks = [(mask == v) for v in self.class_values]
         # mask = np.stack(masks, axis=-1).astype('float')
 
-        # apply augmentations
-        if self.augmentation:
-            sample = self.augmentation(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
+        # # apply augmentations
+        # if self.augmentation:
+        #     sample = self.augmentation(image=image, mask=mask)
+        #     image, mask = sample['image'], sample['mask']
 
         # apply preprocessing
-        if self.preprocessing:
-            sample = self.preprocessing(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
+        # if self.preprocessing:
+        #     sample = self.preprocessing(image=image, mask=mask)
+        #     image, mask = sample['image'], sample['mask']
 
-        return image, mask
+        return image.transpose(2, 0, 1).astype(np.float32)/255, mask
 
     def __len__(self):
         return len(self.ids)
 
 
-import albumentations as albu
+# import albumentations as albu
 
-
-def get_training_augmentation():
-    train_transform = [
-
-        albu.HorizontalFlip(p=0.5),
-
-        # albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
-
-        albu.PadIfNeeded(min_height=512, min_width=512, always_apply=True, border_mode=0),
-        albu.RandomCrop(height=512, width=512, always_apply=True),
-
-        # albu.IAAAdditiveGaussianNoise(p=0.2),
-        # albu.IAAPerspective(p=0.5),
-
-        # albu.OneOf(
-        #     [
-        #         albu.CLAHE(p=1),
-        #         albu.RandomBrightness(p=1),
-        #         albu.RandomGamma(p=1),
-        #     ],
-        #     p=0.9,
-        # ),
-
-        # albu.OneOf(
-        #     [
-        #         # albu.IAASharpen(p=1),
-        #         albu.Blur(blur_limit=3, p=1),
-        #         albu.MotionBlur(blur_limit=3, p=1),
-        #     ],
-        #     p=0.9,
-        # ),
-
-        # albu.OneOf(
-        #     [
-        #         albu.RandomContrast(p=1),
-        #         albu.HueSaturationValue(p=1),
-        #     ],
-        #     p=0.9,
-        # ),
-    ]
-    return albu.Compose(train_transform)
-
-
-def get_validation_augmentation():
-    """Add paddings to make image shape divisible by 32"""
-    test_transform = [
-        albu.PadIfNeeded(384, 480)
-    ]
-    return albu.Compose(test_transform)
+#
+# def get_training_augmentation():
+#     train_transform = [
+#
+#         albu.HorizontalFlip(p=0.5),
+#
+#         # albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+#
+#         albu.PadIfNeeded(min_height=512, min_width=512, always_apply=True, border_mode=0),
+#         albu.RandomCrop(height=512, width=512, always_apply=True),
+#
+#         # albu.IAAAdditiveGaussianNoise(p=0.2),
+#         # albu.IAAPerspective(p=0.5),
+#
+#         # albu.OneOf(
+#         #     [
+#         #         albu.CLAHE(p=1),
+#         #         albu.RandomBrightness(p=1),
+#         #         albu.RandomGamma(p=1),
+#         #     ],
+#         #     p=0.9,
+#         # ),
+#
+#         # albu.OneOf(
+#         #     [
+#         #         # albu.IAASharpen(p=1),
+#         #         albu.Blur(blur_limit=3, p=1),
+#         #         albu.MotionBlur(blur_limit=3, p=1),
+#         #     ],
+#         #     p=0.9,
+#         # ),
+#
+#         # albu.OneOf(
+#         #     [
+#         #         albu.RandomContrast(p=1),
+#         #         albu.HueSaturationValue(p=1),
+#         #     ],
+#         #     p=0.9,
+#         # ),
+#     ]
+#     return albu.Compose(train_transform)
+#
+#
+# def get_validation_augmentation():
+#     """Add paddings to make image shape divisible by 32"""
+#     test_transform = [
+#         albu.PadIfNeeded(384, 480)
+#     ]
+#     return albu.Compose(test_transform)
 
 
 def to_tensor(x, **kwargs):
     return x.transpose(2, 0, 1).astype('float32')
 
 
-def get_preprocessing(preprocessing_fn):
-    """Construct preprocessing transform
-
-    Args:
-        preprocessing_fn (callbale): data normalization function
-            (can be specific for each pretrained neural network)
-    Return:
-        transform: albumentations.Compose
-
-    """
-
-    _transform = [
-        albu.Lambda(image=preprocessing_fn),
-        albu.Lambda(image=to_tensor, mask=to_tensor),
-    ]
-    return albu.Compose(_transform)
+# def get_preprocessing(preprocessing_fn):
+#     """Construct preprocessing transform
+#
+#     Args:
+#         preprocessing_fn (callbale): data normalization function
+#             (can be specific for each pretrained neural network)
+#     Return:
+#         transform: albumentations.Compose
+#
+#     """
+#
+#     _transform = [
+#         albu.Lambda(image=preprocessing_fn),
+#         albu.Lambda(image=to_tensor, mask=to_tensor),
+#     ]
+#     return albu.Compose(_transform)
 
 #### Visualize resulted augmented images and masks
 
@@ -184,8 +184,8 @@ model = smp.MAnet(
 preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
 train_dataset = Dataset(
     x_train_dir,
-    augmentation=get_training_augmentation(),
-    preprocessing=get_preprocessing(preprocessing_fn),
+    # augmentation=get_training_augmentation(),
+    # preprocessing=get_preprocessing(preprocessing_fn),
 )
 
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=0)
